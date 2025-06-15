3353550240bb14c1fe36720a52edbd5108635d27
CerBeer <ocelopp@google.com>
2025-06-16 00:55:30 +0300

    feat: add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerModule } from './logger/logger.module';
 import { HttpExceptionsFilter } from './logger/exceptions/http.exception.filter';
 import { LoggerInterceptor } from './logger/logger.interceptor';
+import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [
@@ -21,10 +23,12 @@ import { LoggerInterceptor } from './logger/logger.interceptor';
     FavsModule,
     PrismaModule,
     LoggerModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
+    { provide: APP_GUARD, useClass: AuthGuard },
     { provide: APP_FILTER, useClass: HttpExceptionsFilter },
     { provide: APP_INTERCEPTOR, useClass: LoggerInterceptor },
   ],
